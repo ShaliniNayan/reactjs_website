11d684da34c5d6454391fa635d473d971f5dfe06
Shalini Nayan <shalininayan@outlook.com>
2025-07-23 18:32:32 +0530

    Install vite react  tailwind

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+// import { useState } from 'react'
+import "./App.css";
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1 h1 class="text-3xl font-bold underline">
+          Hello World
+        </h1>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
